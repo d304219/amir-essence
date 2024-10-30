e489dcdc1eb8d9c5837d341bf198324b13cc7190
d304219 <d304219@edu.curio.nl>
2024-10-30 22:10:49 +0100

    added a header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,25 +1,25 @@
 <header>
     <!-- Logo on the left -->
-    <div class="logo">
-        <a href="#home">
-            <img src="{{ asset('img/logoLight.png') }}" alt="Logo" class="logo-img">
-        </a>
-    </div>
-
-    <!-- Navigation in the center -->
     <nav class="navbar">
-        <a href="#home">Home</a>
-        <a href="#shop">Shop</a>
-        <a href="#about">About Us</a>
+
+        <div class="logo">
+            <a href="#home">
+                <img src="{{ asset('img/logoLight.png') }}" alt="Logo" class="logo-img">
+            </a>
+        </div>
+        <ul>
+            <li>        <a href="#home">Home</a></li>
+            <li>        <a href="#shop">Shop</a></li>
+            <li>        <a href="#about">About Us</a></li>
+        </ul>
+    <!-- Navigation in the center -->
     </nav>
 
     <!-- Icons on the right -->
     <div class="header-icons">
         <a href="#profile">
-            <img src="person-icon.png" alt="User" class="icon">
-        </a>
+            <i class="fa-solid fa-user" style="color: #000000;"></i>        </a>
         <a href="#cart">
-            <img src="shopping-cart.png" alt="Cart" class="icon">
-        </a>
+            <i class="fa-solid fa-cart-shopping" style="color: #000000;"></i>        </a>
     </div>
 </header>
--- resources/views/components/navigation.blade.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar">
-    <a href="#home">Home</a>
-    <a href="#shop">Shop</a>
-    <a href="#about">About Us</a>
-</nav>
