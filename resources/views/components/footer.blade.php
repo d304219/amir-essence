23a0f90ec01daf45934522556581e659e56db175
d304219 <d304219@edu.curio.nl>
2024-10-30 22:11:14 +0100

    added footer and added fonts

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,121 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+
+<footer>
+    <div class="wrapper">
+        <div class="footerGrid">
+            <div class="footerSection">
+                <img src="img/logoLight-removebg.png" alt="Curio Logo" class="footerLogo">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptas impedit fuga facilis, repudiandae eligendi. Facilis odit magni excepturi nulla cumque incidunt neque asperiores deleniti accusamus temporibus esse, perferendis aliquid.</p>
+            </div>
+
+            <div class="footerSection">
+                <h4>Contact</h4>
+                <p>Email: <a href="mailto:info@curio.nl">info@curio.nl</a></p>
+                <p>Tel: +31 76 523 88 00</p>
+            </div>
+
+            <div class="footerSection">
+                <h4>Links</h4>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="#show">Shop</a></li>
+                    <li><a href="#aboutus">About us</a></li>
+                    <li><a href="#faq">Faq</a></li>
+                </ul>
+            </div>
+
+            <div class="footerSection">
+                <h4>Volg Ons</h4>
+                <div class="socialIcons">
+                    <a href="https://www.facebook.com/groups/477785032270977/"><img src="img/facebook-icon.png" alt="Facebook"></a>
+                    <a href="https://x.com/curioonderwijs?lang=en&mx=2"><img src="img/twitter-icon.png" alt="Twitter"></a>
+                    <a href="https://www.instagram.com/curioonderwijsgroep/?hl=en"><img src="img/instagram-icon.png" alt="Instagram"></a>
+                    <a href="https://nl.linkedin.com/school/curioonderwijsgroep/"><img src="img/linkedin-icon.png" alt="LinkedIn"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="footerBottom">
+            <p>&copy; 2024 Curio. Alle rechten voorbehouden.</p>
+        </div>
+    </div>
+</footer>
+
+
+</main>
+
+<footer>
+
+</footer>
+<style>
+
+footer {
+    background-color: #000000;
+    padding: 20px 0;
+    color: #ffffff;
+    font-family: Arial, sans-serif;
+}
+
+.wrapper {
+    width: 80%;
+    margin: 0 auto;
+}
+
+.footerGrid {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+}
+
+.footerSection {
+    flex: 1;
+    margin: 10px;
+}
+
+.footerLogo {
+    max-width: 150px;
+    margin-bottom: 10px;
+}
+
+.footerSection h4 {
+    margin-bottom: 10px;
+    font-size: 18px;
+    text-decoration-line: underline;
+
+}
+
+.footerSection p, .footerSection ul, .footerSection a {
+    font-size: 14px;
+    color: #bdbdbd;
+    text-decoration: none;
+}
+
+.footerSection ul {
+    list-style: none;
+    padding: 0;
+}
+
+.footerSection ul li {
+    margin-bottom: 5px;
+}
+
+.footerSection ul li a:hover {
+    text-decoration: underline;
+}
+
+.socialIcons {
+    display: flex;
+    gap: 10px;
+}
+
+.socialIcons a img {
+    width: 24px;
+    height: 24px;
+}
+
+.footerBottom {
+    text-align: center;
+    margin-top: 20px;
+    font-size: 12px;
+    color: #777;
+}
+</style>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Amir Essence</title>
+    <link rel="icon" href="img/logoLight-removebg.png">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -15,7 +16,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Ekstra:wght@400;700&display=swap" rel="stylesheet">
 
     <link href="/resources/css/app.css" rel="stylesheet">
-
+    <script src="https://kit.fontawesome.com/7fa65373f3.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite('resources/css/app.css')
