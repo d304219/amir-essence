d1711e47a07f0f02c777f71cbecff553e7cb9625
d304219 <d304219@edu.curio.nl>
2024-11-01 02:04:43 +0100

    added hero section on homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.base')
 
 @section('content')
+<section class="hero">
+    <div class="hero-content">
+        <h1>Discover Amir Essence</h1>
+        <p>Embrace sophistication with our exclusive fragrance for men. Unleash a world of elegance and confidence.</p>
+        <a href="#" class="hero-button">Shop Now</a>
+    </div>
+    <div class="hero-image">
+        <img src="img/perfume-mockup.png" alt="Amir Essence Fragrance">
+    </div>
+</section>
 
 @endsection
