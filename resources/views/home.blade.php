e7ce3e367224071e888a8d0ffa75b9a12adefe84
d304219 <d304219@edu.curio.nl>
2024-11-04 22:55:29 +0100

    changed homepage to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<section class="hero">
+    <div class="hero-content">
+        <h1>Discover Amir Essence</h1>
+        <p>Embrace sophistication with our exclusive fragrance for men. Unleash a world of elegance and confidence.</p>
+        <a href="#" class="hero-button">Shop Now</a>
     </div>
-</div>
+    <div class="hero-image">
+        <img src="img/perfume-mockup.png" alt="Amir Essence Fragrance">
+    </div>
+</section>
+
 @endsection
