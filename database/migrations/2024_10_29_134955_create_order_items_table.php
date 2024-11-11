520e82012ef3dc3b2e0b3da3770770b20342b543
d304219 <d304219@edu.curio.nl>
2024-11-11 14:26:55 +0100

    removed order_items migration

--- database/migrations/2024_10_29_134955_create_order_items_table.php
+++ database/migrations/2024_10_29_134955_create_order_items_table.php
@@ -7,18 +7,18 @@ class CreateOrderItemsTable extends Migration
 {
     public function up()
     {
-        Schema::create('order_items', function (Blueprint $table) {
-            $table->id(); // Primary Key
-            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade'); // Foreign Key
-            $table->foreignId('product_id')->constrained('products')->onDelete('cascade'); // Foreign Key
-            $table->integer('amount');
-            $table->decimal('price', 10, 2);
-            $table->timestamps();
-        });
+        // Schema::create('order_items', function (Blueprint $table) {
+        //     $table->id(); // Primary Key
+        //     $table->foreignId('order_id')->constrained('orders')->onDelete('cascade'); // Foreign Key
+        //     $table->foreignId('product_id')->constrained('products')->onDelete('cascade'); // Foreign Key
+        //     $table->integer('amount');
+        //     $table->decimal('price', 10, 2);
+        //     $table->timestamps();
+        // });
     }
 
     public function down()
     {
-        Schema::dropIfExists('order_items');
+        // Schema::dropIfExists('order_items');
     }
 }
