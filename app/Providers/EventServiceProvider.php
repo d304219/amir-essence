59de91a9ff1e67e6e6414a7afb63e1f498e1411d
d304219 <d304219@edu.curio.nl>
2024-11-11 14:28:32 +0100

    changed eventserviceprovider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,24 +9,17 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        Login::class => [
+            LoginListener::class,
         ],
     ];
 
-    /**
-     * Register any events for your application.
-     */
-    public function boot(): void
+    public function boot()
     {
         //
     }
+
 
     /**
      * Determine if events and listeners should be automatically discovered.
