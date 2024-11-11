e413a72f6dcacf8912b306649b52825af74b17c5
d304219 <d304219@edu.curio.nl>
2024-11-11 14:29:42 +0100

    improved web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\CartController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,30 +17,32 @@ use App\Http\Controllers\CartController;
 |
 */
 
+// Home Routes
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('home', function () {
     return view('home');
-});
+})->name('home');
 
+// Dashboard for Admin with auth and admin middleware
 Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', 'admin']], function () {
     Route::resource('products', ProductsController::class);
 });
 
-
+// Cart Routes
 Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
-Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
-Route::post('/cart/update', [CartController::class, 'updateCart'])->name('cart.update');
-Route::post('/cart/remove/{productId}', [CartController::class, 'removeFromCart'])->name('cart.remove');
-Route::post('/cart/clear', [CartController::class, 'clearCart'])->name('cart.clear'); // optional
+Route::get('/cart', [CartController::class, 'view'])->name('cart.view');
+Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
+Route::post('/cart/remove/{productId}', [CartController::class, 'remove'])->name('cart.remove');
 
-
-
+// Products
 Route::get('/perfumes', [ProductsController::class, 'products'])->name('product');
-
 Route::get('/perfumes/{id}', [ProductsController::class, 'show'])->name('product.show');
 
+Route::get('/categories', [CategoriesController::class, 'categories'])->name('categories');
 
+
+// Authentication Routes
 Auth::routes();
